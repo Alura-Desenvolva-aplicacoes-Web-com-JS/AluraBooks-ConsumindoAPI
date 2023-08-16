e6e4c65dcf9ffeb358af78facc94d35465eb7f7d
Murilo <m.coelhos1212@gmail.com>
2023-08-16 09:03:35 -0300

    commit final aula 3
    Nessa aula, aprendi a:
    
    - Tratar promises com seus métodos;
    - Utilizar o método then;
    - Usar o catch para tratar os erros;
    - Usar o finally para mandar uma resposta independente do retorno da promise

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-
-console.log(consultaCEP)
+    .then(resposta => resposta.json())
+    .then(r => {
+        if(r.erro){
+            throw Error('Esse CEP não existe!')
+        }else{
+            console.log(r)
+        }
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento concluído'))
