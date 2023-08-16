86676b8dee13e70a081259f54d8b2660eefe9f87
Murilo <m.coelhos1212@gmail.com>
2023-08-16 09:58:57 -0300

    commit final aula 5
    Nessa aula:
    
    - Coloquei os valores do CEP gerados pela API nos valores dos inputs respectivos.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 async function buscaEndereco(cep){
+    var msgErro = document.querySelector('#erro')
+    msgErro.innerHTML = ""
     try{
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var CEPconvertido = await consultaCEP.json()
         if(CEPconvertido.erro){
             throw Error('CEP não existente')
         }
-        console.log(CEPconvertido)
+        var cidade = document.querySelector('#cidade')
+        var logradouro = document.querySelector('#endereco')
+        var estado = document.querySelector('#estado')
+        var bairro = document.querySelector('#bairro')
+
+        cidade.value = CEPconvertido.localidade
+        logradouro.value = CEPconvertido.logradouro
+        estado.value = CEPconvertido.uf
+        bairro.value = CEPconvertido.bairro
         return CEPconvertido
     } catch(erro){
-        console.log(erro)
+        msgErro.innerHTML = `<p>CEP inválido. Por favor, informe um CEP válido.`
     }
 }
 
+var cep = document.querySelector('#cep')
+cep.addEventListener('focusout', () => buscaEndereco(cep.value))
